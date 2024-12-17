18b0f692b505eeeb009a2d503598bd59438f825f
Alexsadiev <sadievsania@gmail.com>
2024-12-17 09:27:47 +0200

    1
    1

---
+++ практикум 14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практикум_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        //public static void Func()
+        //{
+        //    int x = 1;
+        //    while (x <= 10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //}
+
+        //public static void Func()
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //    while (x <= 10);
+        //}
+        //static void Func()
+        //{
+        //    for(int x=1;x<=10;x++)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+    }
+}
+
+       
