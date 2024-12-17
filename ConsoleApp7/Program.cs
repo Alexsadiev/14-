f08db8e4dd203688563d153373eec3f0790a9567
Alexsadiev <sadievsania@gmail.com>
2024-12-17 09:53:20 +0200

    2
    2

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Func(ref n);
+            Console.ReadLine();
+        }
+        //static void Func(ref int n)
+
+        //{
+        //    int x = 1;
+        //    while (x <= n)
+        //    {
+        //        Console.WriteLine(x)
+        //            x++;
+        //    }
+        //}
+
+        //static void Func(ref int n)
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+
+        //    } while (x <= n);
+        //}
+
+        //static void Func(ref int n)
+        //{
+        //    for (int x = 1; x <=n; x++)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+
+    }
+}
